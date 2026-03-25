4bd5cfaca605fd887e6163bd2f404f0bfaf44b7c
Tomoharu Kawakubo <tomo.kawakubo@gmail.com>
2026-03-25 11:51:30 +0900

    TestAdd修正

--- go/excellent/add_test.go
+++ go/excellent/add_test.go
@@ -7,7 +7,7 @@ import(
 func TestAdd(t *testing.T) {
 	num1 := 8
 	num2 := 5
-	result = Add(num1, num2)
+	result := Add(num1, num2)
 	if result != num1 + num2 {
 		t.Errorf("expected: %v. actual: %v", num1 + num2, result)
 	}
