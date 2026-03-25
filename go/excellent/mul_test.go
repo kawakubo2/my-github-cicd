237a14413719f4a9326ee328d56dc932fd3df384
Tomoharu Kawakubo <tomo.kawakubo@gmail.com>
2026-03-25 15:22:45 +0900

    テスト成功用

--- go/excellent/mul_test.go
+++ go/excellent/mul_test.go
@@ -8,7 +8,7 @@ func TestMul(t *testing.T) {
 	num1 := 4
 	num2 := 3
 	result := Mul(num1, num2)
-	if result != num1 + num2 {
-		t.Errorf("expected: %v: actual: %v", num1 + num2, result)
+	if result != num1 * num2 {
+		t.Errorf("expected: %v: actual: %v", num1 * num2, result)
 	}
 }
